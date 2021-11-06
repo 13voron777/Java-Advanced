1bf7578dbd145bfc9b8bdb1d1e8dd81e44718c4c
Oleg <olvor196@gmail.com>
2021-11-06 11:20:06 +0100

    Commit lesson 4 LR2

--- src/Lesson_4/LAB4/LR_2/Run.java
+++ src/Lesson_4/LAB4/LR_2/Run.java
@@ -1,7 +1,19 @@
 package Lesson_4.LAB4.LR_2;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Run {
     public static void main(String[] args) {
+        String str = "aa aba abba abbba abca abea";
+        String[] words = str.split(" ");
 
+        Pattern pattern = Pattern.compile("ab*a");
+        for (String word : words) {
+            Matcher m = pattern.matcher(word);
+            if (m.matches()) {
+                System.out.println(word);
+            }
+        }
     }
 }
