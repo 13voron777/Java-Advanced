adbea54b51787d03cc831e1ed35827a352b784f7
Oleg <olvor196@gmail.com>
2021-11-18 18:52:34 +0100

    Commit HW7

--- src/Lesson_7/HW7/Task3/Run.java
+++ src/Lesson_7/HW7/Task3/Run.java
@@ -1,24 +1,59 @@
 package Lesson_7.HW7.Task3;
 
-import jakarta.xml.bind.JAXBContext;
-import jakarta.xml.bind.JAXBException;
-import jakarta.xml.bind.Marshaller;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
-import java.io.File;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
 
 public class Run {
     public static void main(String[] args) {
-        Address address = new Address();
-        address.add(new City("Warszawa", "Pl.Constytucji", 3));
-        address.add(new City("Moscow", "Prechistenka", 16));
-
+        final String fileName = "src\\Lesson_7\\HW7\\Task3\\cities.xml";
         try {
-            File file = new File("src\\Lesson_7\\HW7\\Task4\\cities.xml");
-            JAXBContext jaxbContext = JAXBContext.newInstance(Address.class);
-            Marshaller marshaller =jaxbContext.createMarshaller();
-            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-            marshaller.marshal(address, file);
-        } catch (JAXBException e) {
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser saxParser = factory.newSAXParser();
+            DefaultHandler handler = new DefaultHandler() {
+                private String currentElement;
+
+                @Override
+                public void startDocument() throws SAXException {
+                    System.out.println("Start cities.xml");
+                }
+
+                @Override
+                public void endDocument() throws SAXException {
+                    System.out.println("End cities.xml");
+                }
+
+                @Override
+                public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+                    if (attributes.getValue(0) != null) {
+                        System.out.println("Start element " + qName + " " + attributes.getValue(0));
+                    } else {
+                        System.out.println("Start element " + qName);
+                    }
+                    currentElement = qName;
+                }
+
+                @Override
+                public void endElement(String uri, String localName, String qName) throws SAXException {
+                    System.out.println("End element " + qName);
+                    currentElement = null;
+                }
+
+                @Override
+                public void characters(char[] ch, int start, int length) throws SAXException {
+                    if (currentElement != null &&
+                            (currentElement.equalsIgnoreCase("street")
+                                    || currentElement.equalsIgnoreCase("house"))) {
+                        System.out.println(currentElement + ": " + new String(ch, start, length));
+                    }
+                }
+            };
+            saxParser.parse(fileName, handler);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
